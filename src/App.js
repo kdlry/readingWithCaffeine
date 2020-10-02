2ad0f9990ff3f996efeee6b40139fddf1e460c3f
connietee22 <connietsangphotos@gmail.com>
2020-10-02 17:09:41 -0400

    initial work on library section predictive autocomplete

--- src/App.js
+++ src/App.js
@@ -10,38 +10,62 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      userInput: '', // to register change of library selection
-      selectedLibrary: '', // to grab value of library
+      libraryInput: '', // to register change of library selection
+      autoComplete: [], // results from the prediction text
+      selectedLibrary: {}, // to grab value of library
       selectedRadius: '',
       coffeeShops: [],
       distanceBetween: '',
       selectedCoffeeShop: '',
     };
   }
-  componentDidMount(t) {
-    const apiKey = 'rNUBvav2dEGGss4WVvHK64tVGGygn3zB';
-    axios({
-      url: 'http://www.mapquestapi.com/search/v3/prediction',
-      params: {
-        q: 'library',
-        collection: 'poi',
-        key: apiKey,
-      },
-    }).then((res) => {
-      console.log(res.data.results);
-    });
+  componentDidMount() {
+
   }
 
-  handleUserInputChange = (event) => {
-    const userInput = event.target.value;
-    console.log(userInput);
-    this.setState({ userInput });
+  handleLibraryInputChange = (event) => {
+    const libraryInput = event.target.value;
+    console.log(libraryInput);
+
+    // this.setState(stateToSet, callBackOnce-StateToSet-isComplete)
+    this.setState({ libraryInput }, () => {
+
+      if (libraryInput.length >= 3 && libraryInput.length < 25) {
+        const apiKey = 'rNUBvav2dEGGss4WVvHK64tVGGygn3zB';
+
+        axios({
+          url: 'http://www.mapquestapi.com/search/v3/prediction',
+          params: {
+            q: this.state.libraryInput,
+            collection: 'poi',
+            key: apiKey,
+          },
+        })
+          .then((res) => {
+            console.log(res.data.results);
+            this.setState({ autoComplete: [...res.data.results] })
+          });
+      }
+    })
   };
+
+
+
+  handleLibraryInputSelected = (event) => {
+    const selectedLibrary = event.target.value;
+    this.setState({
+      selectedLibrary
+    })
+  }
+
 
   handleFormSubmit = (event) => {
     event.preventDefault();
     console.log('form submitted');
+
+
   };
+
 
   render() {
     return (
@@ -52,6 +76,13 @@ class App extends Component {
           handleUserInputChange={this.handleUserInputChange}
           handleFormSubmit={this.handleFormSubmit}
         />
+        <div>
+          <h2>Results</h2>
+          {this.state.autoComplete.map(results => {
+            return <p key={results.id} onClick>{results.name}</p>
+          })}
+        </div>
+
         <Footer />
       </div>
     );
