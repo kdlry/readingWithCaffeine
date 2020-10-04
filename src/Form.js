8094952c12e49dbf2717337f1628ce60d3cbc3f0
kdlry <kristen.delorey@live.com>
2020-10-04 15:22:16 -0400

    added instructions components, moved transportation input and styled directions

--- src/Form.js
+++ src/Form.js
@@ -5,9 +5,7 @@ const Form = (props) => {
     handleLibraryInputChange,
     libraryInput,
     handleFormSubmit,
-    handleRadiusSelected,
-    modeOfTransportation,
-    handleTransportationChange
+    handleRadiusSelected
   } = props;
   return (
     <>
@@ -44,15 +42,6 @@ const Form = (props) => {
             onChange={handleRadiusSelected}
             placeholder=''
           />
-        </div>
-
-        <div>
-          <label htmlFor="modeOfTransportation">Choose a mode of transportation:</label>
-          <select id="modeOfTransportation" value={modeOfTransportation} onChange={handleTransportationChange}>
-            <option value="fastest">Drive</option>
-            <option value="pedestrian">Walk</option>
-            <option value="bicycle">Bike</option>
-          </select>
         </div>
 
         <div>
