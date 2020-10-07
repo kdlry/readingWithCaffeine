4206397ddc49f4506523251129635fae8f37cee3
raphaelroques <raphaelroques@gmail.com>
2020-10-06 21:30:48 -0400

    Updates

---
+++ precache-manifest.5c896a9a4f1f3530d112a4496324e06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f20f5e80c2262d3e08b2e9c257ccb9",
+    "url": "/readingWithCaffeine/index.html"
+  },
+  {
+    "revision": "f321de25d64c08b9d18d",
+    "url": "/readingWithCaffeine/static/css/main.a9176e11.chunk.css"
+  },
+  {
+    "revision": "ccb1d10968832143accc",
+    "url": "/readingWithCaffeine/static/js/2.500e5cd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/readingWithCaffeine/static/js/2.500e5cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f321de25d64c08b9d18d",
+    "url": "/readingWithCaffeine/static/js/main.8f0c4575.chunk.js"
+  },
+  {
+    "revision": "4ce24b48c8ad9aded873",
+    "url": "/readingWithCaffeine/static/js/runtime-main.00093b2b.js"
+  },
+  {
+    "revision": "ff47478885f6f5db979d790cd6dc934a",
+    "url": "/readingWithCaffeine/static/media/thomas-park-unsplash.ff474788.png"
+  }
+]);
